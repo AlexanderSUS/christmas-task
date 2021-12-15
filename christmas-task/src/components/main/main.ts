803ba58e42516b11475ab581686fd8a2e376802f
Alexander Suslov <sasha_suslov07@mail.ru>
2021-12-15 17:44:48 +0300

    feat: add main element and update common styles

---
+++ christmas-task/src/components/main/main.ts
@@ -0,0 +1,6 @@
+import './index.scss';
+
+const main = document.createElement('main');
+main.classList.add('main');
+
+export default main;
--- christmas-task/src/index.ts
+++ christmas-task/src/index.ts
@@ -1,8 +1,10 @@
 import './style/index.scss';
 import header from './components/header/header';
+import main from './components/main/main';
 import footer from './components/footer/footer';
 
 const root = document.getElementById('root');
 root?.appendChild(header);
+root?.appendChild(main);
 root?.appendChild(footer);
 
