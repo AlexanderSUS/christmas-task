a30dff1d8d2ef3d2e42d0f7dba87ab20381034c5
Alexander Suslov <sasha_suslov07@mail.ru>
2021-12-15 22:56:16 +0300

    refactor: refactor footer code

--- christmas-task/src/components/footer/footer.ts
+++ christmas-task/src/components/footer/footer.ts
@@ -5,46 +5,21 @@ import GitHubLogo from './assets/github.svg';
 const footer = document.createElement('footer');
 footer.classList.add('footer');
 
-const copyRigth = document.createElement('span');
-copyRigth.classList.add('footer__text');
-copyRigth.innerHTML= '&copy;';
+footer.innerHTML = `<div class="footer__info">
+                      <span class="footer__text">©</span>
+                      <span class="footer__text">2021</span>
+                      <span class="footer__text">Alexander Suslov</span>
+                      <a class="footer__link footer__link_git" href="https://github.com/AlexanderSUS"></a>
+                    </div>
+                    <a class="footer__link footer__link_rss" href="https://rs.school/"></a>`;
 
-const year = document.createElement('span');
-year.classList.add('footer__text');
-year.textContent = '2021';
+const imageUrlLis = [GitHubLogo, RSSLogo];
 
-const author = document.createElement('span');
-author.classList.add('footer__text');
-author.textContent = 'Alexander Suslov';
-
-const gitHubLink = document.createElement('a');
-gitHubLink.classList.add('footer__link');
-gitHubLink.setAttribute('href', 'https://github.com/AlexanderSUS');
-
-const gitHubImg  = document.createElement('img'); 
-gitHubImg.classList.add('footer__img');
-gitHubImg.src = GitHubLogo;
-
-gitHubLink.appendChild(gitHubImg);
-
-const infoContainer = document.createElement('div');
-infoContainer.classList.add('footer__info');
-infoContainer.appendChild(copyRigth);
-infoContainer.appendChild(year);
-infoContainer.appendChild(author);
-infoContainer.appendChild(gitHubLink);
-
-const rssLogo = document.createElement('img');
-rssLogo.src = RSSLogo;
-rssLogo.classList.add('footer__img');
-
-const rssLink = document.createElement('a');
-rssLink.setAttribute('href', 'https://rs.school/');
-rssLink.classList.add('footer__link');
-rssLink.appendChild(rssLogo);
-
-
-footer.appendChild(infoContainer);
-footer.appendChild(rssLink);
+footer.querySelectorAll('.footer__link').forEach((link, index) => {
+  const image = document.createElement('img');
+  image.classList.add('footer__img');
+  image.src = imageUrlLis[index];
+  link.appendChild(image);
+});
 
 export default footer;
