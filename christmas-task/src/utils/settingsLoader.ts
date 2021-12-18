b1bfb0c59ceb5e306e7a78af57f60507fecc4697
Alexander Suslov <sasha_suslov07@mail.ru>
2021-12-18 04:32:54 +0300

    feat: add class settings

--- christmas-task/src/utils/settingsLoader.ts
+++ christmas-task/src/utils/settingsLoader.ts
@@ -1,10 +1,53 @@
-import header from "../components/header/header";
-import main from "../components/main/main";
-import footer from "../components/footer/footer";
-import { ToyCard } from "../components/toyCard/toyCard";
+import { Toy } from '../appData/data';
 
-class page {
-  constructor(parameters) {
-    
+type MinMax = {
+    minRange: number;
+    maxRange: number;
+};
+
+type Values = { colors: boolean[]; shapes: boolean[]; sizes: boolean[] };
+
+interface SettingsType {
+    values: Values;
+    ranges: MinMax[];
+    favourite: boolean[];
+}
+
+export interface SettingsTypeClassInt {
+  settings: SettingsType;
+  loadSettings(): void;
+}
+
+export class Settings  implements SettingsTypeClassInt {
+
+  settings: SettingsType;
+
+  constructor(data: Toy[]) {
+
+    this.settings =  {
+      values: {
+        colors: new Array(5).fill(false) as Array<boolean>,
+        shapes: new Array(5).fill(false) as Array<boolean>,
+        sizes: new Array(3).fill(false) as Array<boolean>,
+      },
+      ranges: [
+        {
+          minRange: Math.min(...data.map(({ count }) => +count)),
+          maxRange: Math.max(...data.map(({ count }) => +count)),
+        },
+        {
+          minRange: Math.min(...data.map(({ year }) => +year)),
+          maxRange: Math.max(...data.map(({ year }) => +year)),
+        },
+      ],
+      favourite: new Array(60).fill(false),
+    }
+  }
+
+  loadSettings() {
+    const savedSettings = localStorage.getItem('christmas-settings');
+    if (savedSettings !== null) {
+      this.settings = JSON.parse(savedSettings);
+    }
   }
 }
