fb8df6ee32935427cdc8c0424530cc6a82a97898
Alexander Suslov <sasha_suslov07@mail.ru>
2021-12-27 05:54:56 +0300

    feat: add color control to garland

--- christmas-task/src/components/ligthtsSwitch.ts
+++ christmas-task/src/components/ligthtsSwitch.ts
@@ -14,14 +14,18 @@ export class LightsSwitch implements LightsSwitchInt {
 
   switch: HTMLInputElement | null;
 
+  garland: HTMLInputElement | null;
+
   constructor(parent: HTMLElement, appData: AppDataInt) {
     this.parent = parent;
     this.appData = appData;
     this.buttons = parent.querySelectorAll('.light-button');
     this.switch = parent.querySelector('.light-switch__input');
+    this.garland = this.parent.querySelector('.lights-container');
   }
 
   init() {
+    this.turnOnDefault();
     this.switchColorListener();
     this.turnLightsListener();
   }
@@ -29,32 +33,52 @@ export class LightsSwitch implements LightsSwitchInt {
   private switchColorListener() {
     this.buttons.forEach((btn) => {
       btn.addEventListener('click', (e) => {
-        this.turnOffAll();
-        const selected = e.target as HTMLElement;
-        const selectedNum = selected.getAttribute('data-num');
+        this.turnOffBtnsAll();
 
-        selected.classList.add('active');
+        const selectedButton = e.target as HTMLElement;
+        const color = selectedButton.getAttribute('data-num');
+        selectedButton.classList.add('active');
+
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        this.appData.ligths.color = +selectedNum!;
+        this.switchGarlandColor(+color!);
       });
     });
   }
 
-  private turnOffAll() {
+  private turnOffBtnsAll() {
     this.buttons.forEach((btn) => {
       btn.classList.remove('active');
     });
   }
 
+  private turnOnDefault() {
+    this.buttons[this.appData.ligths.color].classList.add('active');
+    this.garland?.classList.add(`color${this.appData.ligths.color}`);
+  }
+
   private turnLightsListener() {
     this.switch?.addEventListener('change', () => {
       if (this.switch?.checked) {
-        this.parent.querySelector('.lights-container')?.classList.add('active');
-        this.appData.ligths.isOn = true;
+        this.turnOnGarland();
       } else {
-        this.parent.querySelector('.lights-container')?.classList.remove('active');
-        this.appData.ligths.isOn = false;
+        this.turnOffGarland();
       }
     });
+  }
+
+  private turnOnGarland() {
+    this.garland?.classList.add('active');
+    this.appData.ligths.isOn = true;
+  }
+
+  private turnOffGarland() {
+    this.garland?.classList.remove('active');
+    this.appData.ligths.isOn = false;
+  }
+
+  switchGarlandColor(color: number) {
+    this.garland?.classList.remove(`color${this.appData.ligths.color}`);
+    this.appData.ligths.color = color;
+    this.garland?.classList.add(`color${this.appData.ligths.color}`);
   }
 }
